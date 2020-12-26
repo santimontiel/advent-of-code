f42824e8da901bcf52c6210997a38f274681e9af
SantiMontiel <santiagomontiel@outlook.com>
2020-12-27 00:50:55 +0100

    Ex 4, part 1 done

--- day_04_passport_processing.py
+++ day_04_passport_processing.py
@@ -12,15 +12,44 @@ import os
 
 fp = open('day_04_input.txt', 'r')
 
-lista = []
+# Parseo el fichero, pongo todos los datos en una única línea.
+# Cada línea comienza con un espacio.
+
+reg_i = ""
+reg = []
 line = fp.readline()
 
 while line:
-    lista.append(line.strip())
-    line = fp.readline()
+    
+    if (len(line) > 1):
+        reg_i = reg_i + " " + line[:-1]
+    else:
+        reg.append(reg_i)
+        # print(reg_i)
+        reg_i = ""
+        # print("*****")
+                
+    line = fp.readline()    
+
+print(len(reg))
 
 fp.close()
 
-# -- Parte 1
-passport = []
-id = {}
+# Si hay 8 parámetros -> válido y si hay 7 parámetros, pero no esta 'cid' -> válido
+valido, fallo = 0, 0
+
+for i in range(len(reg)):
+    
+    print(reg[i])
+
+    if ((reg[i].count(" ") == 8) or (reg[i].count(" ") == 7 and reg[i].find("cid:") == -1)):
+        valido += 1
+    else:
+        fallo += 1
+    
+    print("valido :" + str(valido) + ", falso: " + str(fallo))
+    print("------------------------------------------------")
+    
+
+print("Hay " + str(valido) + " pasaportes válidos y " + str(fallo) + " pasaportes inválidos.")
+
